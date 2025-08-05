78a9d94cdbe0e3ab9d73e431009202d0234df8dd
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-08-05 17:48:49 -0500

    ✨ Creación de AppRoutes para el manejo de rutas

---
+++ citas-medicas/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+// Importamos los componentes de React Router necesarios:
+// - Routes: contenedor de todas las rutas
+// - Route: define una ruta individual
+// - Navigate: redirige a otra ruta si se cumple una condición
+import { Routes, Route, Navigate } from 'react-router-dom';
+// Importamos las "páginas" (componentes de rutas)
+
+
+const AppRoutes = () => (
+    <Routes>
+    </Routes>
+);
+
+
+export default AppRoutes;
