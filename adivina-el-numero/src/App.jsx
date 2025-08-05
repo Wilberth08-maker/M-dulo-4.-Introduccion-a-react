d98e286b066ed855a5c604aaaa42bd2f96a0296b
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-08-05 14:42:12 -0500

    🚀 Importación de componente game en app.jsx

--- adivina-el-numero/src/App.jsx
+++ adivina-el-numero/src/App.jsx
@@ -1,8 +1,10 @@
-
+import Game from './components/Game'
 function App() {
 
   return (
-    <h1>Funciona</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-950">
+      <Game/>
+    </div>
   )
 }
 
